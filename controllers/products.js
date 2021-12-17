57a5d9b40250f14685d4ec92cd6353f0c0d8521d
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-12-17 11:35:58 +0300

    page filter added

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(5);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -28,13 +32,19 @@ const getAllProducts = async (req, res) => {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
-    result = result.sort("createdAt")
+    result = result.sort("createdAt");
   }
 
   if (fields) {
-    const fieldsList = fields.split(',').join(' ');
+    const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
