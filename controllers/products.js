2749b99dde29e84b7ea02e833f7468ac10485803
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-12-17 10:59:00 +0300

    select and sort query added

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,46 @@
-
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'products testing route'})
-}
+  const products = await Product.find({}).select('name price');
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
-}
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt")
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 module.exports = {
-    getAllProductsStatic,
-    getAllProducts
-}
+  getAllProductsStatic,
+  getAllProducts,
+};
--- populate.js
+++ populate.js
@@ -6,9 +6,13 @@ const jsonProduct = require("./products.json");
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProduct);
     console.log("Success!!!");
+    process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
